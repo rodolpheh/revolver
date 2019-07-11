0c9582c75878fd004a209912bcaa31e3723e5f50
rodolpheh <houdas.rodolphe@gmail.com>
2019-07-11 20:18:43 +0100

    Moving logic code into seperate file

--- flask_app.py
+++ flask_app.py
@@ -1,57 +1,33 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
-import metric_time
 import datetime
-from dateutil.parser import isoparse
 import pytz
 
+from revolver.revolver import Revolver
+
 app = Flask(__name__)
+
+year_offset = 11792
 
 @app.route('/')
 def hello_world():
     return "Site in construction"
 
-@app.route('/convert/<date>')
-def convert(date):
-    return date
-
 @app.route('/now')
 def now():
-    now = datetime.datetime.now(pytz.timezone('Europe/Paris'))
-    return to_revolver(now)
+    return str(Revolver.now())
 
 @app.route('/from_iso/<date_str>')
 def from_iso(date_str):
-    iso_date = isoparse(date_str)
-    return to_revolver(iso_date)
+    return str(Revolver.from_iso_str(date_str))
 
 @app.route('/to_iso/<revolver>')
 def to_iso(revolver):
+    # Parse the good stuff
     date_str, time_str = revolver.split('-')
     year_str, month_str, day_str = date_str.split('.')
-    year = int(year_str) - 10000
-    month = 1
-    day = 1
-    all_minutes = (int(time_str[0]) * 2.4 * 60) + (int(time_str[1:]) * 1.44)
-    hour = int(all_minutes / 60)
-    minutes = int(all_minutes) % 60
-    full = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minutes)
-    full = pytz.timezone('Europe/Paris').localize(full)
-    return full.isoformat()
+    hour, minute = int(time_str[0]), int(time_str[1:])
+    return Revolver(int(year_str), int(month_str), int(day_str), hour, minute).iso_date
 
-def to_revolver(iso_date):
-    #now = datetime.datetime.now(pytz.timezone('Europe/Paris'))
-    if iso_date.tzinfo is None or iso_date.tzinfo.utcoffset(iso_date) is None:
-        iso_date = iso_date.replace(tzinfo=pytz.timezone('Europe/Paris'))
-    else:
-        iso_date = iso_date.astimezone(pytz.timezone('Europe/Paris'))
-    time = metric_time.DecimalTime().decimal_time(iso_date)
-    date = metric_time.RepublicanCalendar().republican_date(iso_date)
-    year = iso_date.year + 10000
-    month = metric_time.RepublicanCalendar().MONTHS.index(date.month)
-    date_str = "{}.{:02d}.{:02d}".format(year, month, date.day)
-    time_str = "{}{:02d}".format(time.hours, time.minutes)
-    return "{}-{}".format(date_str, time_str)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
---
+++ revolver/revolver.py
@@ -0,0 +1,101 @@
+import metric_time
+import datetime
+from dateutil.parser import isoparse
+import pytz
+
+year_offset = 11792
+
+class Revolver(object):
+  
+  def __init__(self, year, month, day, hour, minute, *args, **kwargs):
+    self.year = year
+    self.month = month
+    self.day = day
+
+    self.hour = hour
+    self.minute = minute
+
+  @property
+  def yearly(self):
+    return str(self.year)
+
+  @property
+  def monthly(self):
+    return "{}.{:02d}".format(self.year, self.month)
+
+  @property
+  def dayly(self):
+    return "{}.{:02d}.{:02d}".format(self.year, self.month, self.day)
+
+  @property
+  def hourly(self):
+    return "{}.{:02d}.{:02d}.{}{:02d}".format(self.year, self.month, self.day, self.hour, self.minute)
+
+  def __str__(self):
+    return self.hourly
+
+  def __repr__(self):
+    return "Revolver(year={}, month={}, day={}, hour={}, minute={})".format(self.year, self.month, self.day, self.hour, self.minute)
+
+  @staticmethod
+  def from_iso_str(date_str):
+    iso_date = isoparse(date_str)
+    return Revolver.from_iso(iso_date)
+
+  @staticmethod
+  def from_iso(iso_date):
+    if iso_date.tzinfo is None or iso_date.tzinfo.utcoffset(iso_date) is None:
+        #iso_date = iso_date.replace(tzinfo=pytz.timezone('Europe/London'))
+        iso_date = iso_date.astimezone(pytz.timezone('UTC'))
+    elif iso_date.tzinfo.utcoffset(iso_date) is not None:
+        iso_date -= iso_date.tzinfo.utcoffset(iso_date)
+    #else:
+    #    iso_date = iso_date.astimezone(pytz.timezone('UTC'))
+
+    time = metric_time.DecimalTime().decimal_time(iso_date)
+    date = metric_time.RepublicanCalendar().republican_date(iso_date)
+    year = int(date.year) + year_offset
+    month = metric_time.RepublicanCalendar().MONTHS.index(date.month) + 1
+
+    revolver = Revolver(year, month, date.day, time.hours, time.minutes)
+    return revolver
+
+  @staticmethod
+  def now():
+    now = datetime.datetime.now(pytz.timezone('UTC'))
+    return Revolver.from_iso(now)
+
+  @property
+  def iso_date(self):
+    republican_year = self.year - year_offset
+    year = self.year - year_offset + 1792
+
+    # Calculations source :
+    # https://fr.wikipedia.org/wiki/Concordance_des_dates_des_calendriers_r%C3%A9publicain_et_gr%C3%A9gorien
+
+    temp_date = datetime.datetime(year=year, month=9, day=21, tzinfo=datetime.timezone.utc)
+
+    # If the date is between Ventose 1st and Nivose 11th, subtract one year
+    if self.month < 4 or (self.month == 4 and self.day <= 11):
+        temp_date.year -= 1
+
+        # If the year is a multiple of 4, add a day
+        if republican_year % 4 == 0:
+            temp_date += datetime.timedelta(days=1)
+
+    time_since_sans_culottides = (self.month - 1) * 30 + (self.day - 1)
+
+    temp_date += datetime.timedelta(days=time_since_sans_culottides)
+
+    for century in range(18, int(temp_date.year / 100)):
+        if century % 4 == 0:
+            continue
+        if temp_date > datetime.datetime(year=temp_date.year, month=2, day=28, tzinfo=datetime.timezone.utc):
+            temp_date += datetime.timedelta(days=1)
+
+    # Compute the time (accurate to 1 minute because we don't keep the seconds in the format)
+    minutes = (self.hour * 2.4 * 60) + (self.minute * 1.44)
+    hour = int(minutes / 60)
+    minutes = int(minutes) % 60
+    date = datetime.datetime(year=year, month=temp_date.month, day=temp_date.day, hour=hour, minute=minutes, tzinfo=datetime.timezone.utc)
+    return date.isoformat()
